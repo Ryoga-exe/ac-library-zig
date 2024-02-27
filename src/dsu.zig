07210ef773b1ea744e0eab70d491d7b8e3020fe2
Ryoga-exe <git@ryoga.dev>
2024-02-27 11:18:43 +0900

    feat: implement Dsu.groups

--- src/dsu.zig
+++ src/dsu.zig
@@ -47,9 +47,70 @@ pub const Dsu = struct {
         const x = self.leader(a);
         return @intCast(-self.parent_or_size[x]);
     }
-    // pub fn groups(self: *Self) type {
-    //     _ = self;
-    // }
+    pub fn groups(self: *Self) !Groups {
+        var group_index = try self.allocator.alloc(?usize, self.n);
+        defer self.allocator.free(group_index);
+        @memset(group_index, null);
+        var index: usize = 0;
+        for (0..self.n) |i| {
+            const leader_buf = self.leader(i);
+            if (group_index[leader_buf] == null) {
+                group_index[leader_buf] = index;
+                group_index[i] = index;
+                index += 1;
+            } else {
+                group_index[i] = group_index[leader_buf];
+            }
+        }
+        return try Groups.init(self.allocator, index, group_index);
+    }
+};
+
+const Groups = struct {
+    const Self = @This();
+
+    len: usize,
+    data: []usize,
+    group_size: []usize,
+    offset: []usize,
+    allocator: Allocator,
+
+    pub fn init(allocator: Allocator, group_num: usize, group_index: []const ?usize) !Self {
+        var self = Self{
+            .len = group_num,
+            .data = try allocator.alloc(usize, group_index.len),
+            .group_size = try allocator.alloc(usize, group_num),
+            .offset = try allocator.alloc(usize, group_num),
+            .allocator = allocator,
+        };
+        @memset(self.group_size, 0);
+        for (group_index) |index| {
+            self.group_size[index.?] += 1;
+        }
+        self.offset[0] = 0;
+        for (1..group_num) |i| {
+            self.offset[i] = self.offset[i - 1] + self.group_size[i - 1];
+        }
+        var index = try allocator.alloc(usize, group_num);
+        defer allocator.free(index);
+        @memset(index, 0);
+        for (0..group_index.len) |i| {
+            const group = group_index[i].?;
+            const offset = self.offset[group];
+            self.data[offset + index[group]] = i;
+            index[group] += 1;
+        }
+        return self;
+    }
+    pub fn deinit(self: *Self) void {
+        self.allocator.free(self.data);
+        self.allocator.free(self.group_size);
+        self.allocator.free(self.offset);
+    }
+    pub fn get(self: *Self, a: usize) []usize {
+        const offset = self.offset[a];
+        return self.data[offset .. offset + self.group_size[a]];
+    }
 };
 
 test "Dsu works" {
@@ -62,5 +123,10 @@ test "Dsu works" {
     try std.testing.expect(d.same(0, 2));
     try std.testing.expect(d.size(0) == 3);
     try std.testing.expect(!d.same(0, 3));
-    // std.testing.expectEqualSlices(...):
+
+    var groups = try d.groups();
+    defer groups.deinit();
+    try std.testing.expect(groups.len == 2);
+    try std.testing.expectEqualSlices(usize, &[_]usize{ 0, 1, 2 }, groups.get(0));
+    try std.testing.expectEqualSlices(usize, &[_]usize{3}, groups.get(1));
 }
