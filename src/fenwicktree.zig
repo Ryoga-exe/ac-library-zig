782fc9e9453e8a0fe993396aa5b451c89c952f67
Ryoga-exe <git@ryoga.dev>
2024-03-30 13:23:09 +0900

    feat: reimplement fenwicktree

--- src/fenwicktree.zig
+++ src/fenwicktree.zig
@@ -1,46 +1,53 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
 
-pub const FenwickTree = struct {
-    const Self = @This();
-    const T = i64;
+pub fn FenwickTreeI(comptime T: type, comptime e: T, comptime op: fn (T, T) T) type {
+    return struct {
+        const Self = @This();
 
-    n: usize,
-    data: []T,
-    allocator: Allocator,
+        n: usize,
+        data: []T,
+        allocator: Allocator,
 
-    pub fn init(allocator: Allocator, n: usize) !Self {
-        var self = Self{
-            .n = n,
-            .data = try allocator.alloc(T, n),
-            .allocator = allocator,
-        };
-        @memset(self.data, 0);
-        return self;
-    }
-    pub fn deinit(self: *Self) void {
-        self.allocator.free(self.data);
-    }
-    pub fn add(self: *Self, idx: usize, val: T) void {
-        var p = idx + 1;
-        while (p <= self.n) {
-            self.data[p - 1] += val;
-            p += p & (~p +% 1);
+        pub fn init(allocator: Allocator, n: usize) !Self {
+            var self = Self{
+                .n = n,
+                .data = try allocator.alloc(T, n),
+                .allocator = allocator,
+            };
+            @memset(self.data, e);
+            return self;
         }
-    }
-    pub fn sum(self: *Self, l: usize, r: usize) T {
-        return self.accum(r) - self.accum(l);
-    }
-    fn accum(self: *Self, idx: usize) T {
-        var r = idx;
-        var s = @as(T, 0);
-        while (r > 0) {
-            s += self.data[r - 1];
-            r &= r - 1;
+        pub fn deinit(self: *Self) void {
+            self.allocator.free(self.data);
         }
-        return s;
-    }
-};
+        pub fn add(self: *Self, idx: usize, val: T) void {
+            var p = idx + 1;
+            while (p <= self.n) {
+                self.data[p - 1] = op(self.data[p - 1], val);
+                p += p & (~p +% 1);
+            }
+        }
+        pub fn sum(self: *Self, l: usize, r: usize) T {
+            return self.accum(r) - self.accum(l);
+        }
+        fn accum(self: *Self, idx: usize) T {
+            var r = idx;
+            var s = @as(T, e);
+            while (r > 0) {
+                s += self.data[r - 1];
+                r &= r - 1;
+            }
+            return s;
+        }
+    };
+}
+
+pub const FenwickTree = FenwickTreeI(i64, 0, addition);
+
+fn addition(x: i64, y: i64) i64 {
+    return x + y;
+}
 
 test "FenwickTree works" {
     const allocator = std.testing.allocator;
