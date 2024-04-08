34648f49101e6a81662c77e941e34274a8828fd2
Ryoga-exe <git@ryoga.dev>
2024-04-08 18:19:03 +0900

    feat: add internal_scc

---
+++ src/internal_scc.zig
@@ -0,0 +1,37 @@
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+
+pub const SccGraph = struct {
+    const Self = @This();
+
+    n: usize,
+    edges: []std.ArrayList(usize),
+    allocator: Allocator,
+
+    pub fn init(allocator: Allocator, n: usize) Self {
+        var self = Self{
+            .n = n,
+            .edges = try allocator.alloc(std.ArrayList(usize), n),
+            .allocator = allocator,
+        };
+        for (self.edges) |edge| {
+            edge = try std.ArrayList(usize).init(allocator);
+        }
+        return self;
+    }
+    pub fn deinit(self: *Self) void {
+        for (self.edges) |edge| {
+            edge.deinit();
+        }
+        self.allocator.free(self.edges);
+    }
+    pub fn numVertices(self: Self) usize {
+        return self.n;
+    }
+    pub fn addEdge(self: *Self, from: usize, to: usize) !void {
+        try self.edges[from].append(to);
+    }
+    pub fn sccIds(self: Self) std.meta.Tuple(&.{ usize, usize }) {
+        _ = self;
+    }
+};
--- src/scc.zig
+++ src/scc.zig
@@ -1,15 +1,21 @@
 const std = @import("std");
+const internal = @import("internal_scc.zig");
 const Allocator = std.mem.Allocator;
 
 pub const SccGraph = struct {
     const Self = @This();
 
-    allocator: Allocator,
+    internal: internal.SccGraph,
 
-    pub fn init(allocator: Allocator) Self {
+    pub fn init(allocator: Allocator, n: usize) Self {
         return Self{
-            .allocator = allocator,
+            .internal = try internal.SccGraph.init(allocator, n),
         };
     }
-    pub fn deinit() void {}
+    pub fn deinit(self: *Self) void {
+        self.internal.deinit();
+    }
+    pub fn addEdge(self: *Self, from: usize, to: usize) !void {
+        try self.internal.addEdge(from, to);
+    }
 };
