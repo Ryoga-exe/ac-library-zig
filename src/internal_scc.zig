a18c0ba768c146bcecaab38eaa0d7372d9b47dd8
Ryoga-exe <git@ryoga.dev>
2024-04-09 21:34:55 +0900

    feat: implement internal_scc

--- src/internal_scc.zig
+++ src/internal_scc.zig
@@ -71,11 +71,18 @@ pub const SccGraph = struct {
         }
         return .{ env.group_num, self.allocator.dupe(usize, env.ids) };
     }
-    pub fn scc(self: Self) void {
+    pub fn scc(self: Self) !Groups {
         const ids = self.sccIds();
         defer self.allocator.free(ids.@"1");
         const group_num = ids.@"0";
-        _ = group_num;
+
+        var group_index = self.allocator.alloc(?usize, self.n);
+        defer self.allocator.free(group_index);
+        @memset(group_index, 0);
+        for (0..self.n) |i| {
+            group_index[i] = ids.@"1"[i];
+        }
+        return try Groups.init(self.allocator, group_num, group_index);
     }
     fn dfs(v: usize, n: usize, env: *Env) !void {
         env.low[v] = env.now_ord;
@@ -104,5 +111,52 @@ pub const SccGraph = struct {
             }
             env.group_num += 1;
         }
+    }
+};
+
+const Groups = struct {
+    const Self = @This();
+
+    len: usize,
+    data: []usize,
+    group_size: []usize,
+    offset: []usize,
+    allocator: Allocator,
+
+    pub fn init(allocator: Allocator, group_num: usize, group_index: []const ?usize) !Self {
+        var self = Self{
+            .len = group_num,
+            .data = try allocator.alloc(usize, group_index.len),
+            .group_size = try allocator.alloc(usize, group_num),
+            .offset = try allocator.alloc(usize, group_num),
+            .allocator = allocator,
+        };
+        @memset(self.group_size, 0);
+        for (group_index) |index| {
+            self.group_size[index.?] += 1;
+        }
+        self.offset[0] = 0;
+        for (1..group_num) |i| {
+            self.offset[i] = self.offset[i - 1] + self.group_size[i - 1];
+        }
+        var index = try allocator.alloc(usize, group_num);
+        defer allocator.free(index);
+        @memset(index, 0);
+        for (0..group_index.len) |i| {
+            const group = group_index[i].?;
+            const offset = self.offset[group];
+            self.data[offset + index[group]] = i;
+            index[group] += 1;
+        }
+        return self;
+    }
+    pub fn deinit(self: *Self) void {
+        self.allocator.free(self.data);
+        self.allocator.free(self.group_size);
+        self.allocator.free(self.offset);
+    }
+    pub fn get(self: *Self, a: usize) []usize {
+        const offset = self.offset[a];
+        return self.data[offset .. offset + self.group_size[a]];
     }
 };
