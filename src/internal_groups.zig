bef79ecc70a1e490d143792a12bd9c506fb375f6
Ryoga-exe <git@ryoga.dev>
2024-04-09 21:38:47 +0900

    feat: add internal_groups.zig

---
+++ src/internal_groups.zig
@@ -0,0 +1,49 @@
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+
+pub const Groups = struct {
+    const Self = @This();
+
+    len: usize,
+    data: []usize,
+    group_size: []usize,
+    offset: []usize,
+    allocator: Allocator,
+
+    pub fn init(allocator: Allocator, group_num: usize, group_index: []const ?usize) !Self {
+        var self = Self{
+            .len = group_num,
+            .data = try allocator.alloc(usize, group_index.len),
+            .group_size = try allocator.alloc(usize, group_num),
+            .offset = try allocator.alloc(usize, group_num),
+            .allocator = allocator,
+        };
+        @memset(self.group_size, 0);
+        for (group_index) |index| {
+            self.group_size[index.?] += 1;
+        }
+        self.offset[0] = 0;
+        for (1..group_num) |i| {
+            self.offset[i] = self.offset[i - 1] + self.group_size[i - 1];
+        }
+        var index = try allocator.alloc(usize, group_num);
+        defer allocator.free(index);
+        @memset(index, 0);
+        for (0..group_index.len) |i| {
+            const group = group_index[i].?;
+            const offset = self.offset[group];
+            self.data[offset + index[group]] = i;
+            index[group] += 1;
+        }
+        return self;
+    }
+    pub fn deinit(self: *Self) void {
+        self.allocator.free(self.data);
+        self.allocator.free(self.group_size);
+        self.allocator.free(self.offset);
+    }
+    pub fn get(self: *Self, a: usize) []usize {
+        const offset = self.offset[a];
+        return self.data[offset .. offset + self.group_size[a]];
+    }
+};
--- src/internal_scc.zig
+++ src/internal_scc.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const Csr = @import("internal_csr.zig").Csr;
+const Groups = @import("internal_groups.zig").Groups;
 const Allocator = std.mem.Allocator;
 
 pub const SccGraph = struct {
@@ -111,52 +112,5 @@ pub const SccGraph = struct {
             }
             env.group_num += 1;
         }
-    }
-};
-
-const Groups = struct {
-    const Self = @This();
-
-    len: usize,
-    data: []usize,
-    group_size: []usize,
-    offset: []usize,
-    allocator: Allocator,
-
-    pub fn init(allocator: Allocator, group_num: usize, group_index: []const ?usize) !Self {
-        var self = Self{
-            .len = group_num,
-            .data = try allocator.alloc(usize, group_index.len),
-            .group_size = try allocator.alloc(usize, group_num),
-            .offset = try allocator.alloc(usize, group_num),
-            .allocator = allocator,
-        };
-        @memset(self.group_size, 0);
-        for (group_index) |index| {
-            self.group_size[index.?] += 1;
-        }
-        self.offset[0] = 0;
-        for (1..group_num) |i| {
-            self.offset[i] = self.offset[i - 1] + self.group_size[i - 1];
-        }
-        var index = try allocator.alloc(usize, group_num);
-        defer allocator.free(index);
-        @memset(index, 0);
-        for (0..group_index.len) |i| {
-            const group = group_index[i].?;
-            const offset = self.offset[group];
-            self.data[offset + index[group]] = i;
-            index[group] += 1;
-        }
-        return self;
-    }
-    pub fn deinit(self: *Self) void {
-        self.allocator.free(self.data);
-        self.allocator.free(self.group_size);
-        self.allocator.free(self.offset);
-    }
-    pub fn get(self: *Self, a: usize) []usize {
-        const offset = self.offset[a];
-        return self.data[offset .. offset + self.group_size[a]];
     }
 };
