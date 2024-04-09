ea44aa1b295f4b4391188675e29cdf4a02b139ac
Ryoga-exe <git@ryoga.dev>
2024-04-09 16:17:40 +0900

    feat: implement Csr

--- src/internal_csr.zig
+++ src/internal_csr.zig
@@ -6,11 +6,13 @@ pub fn Csr(comptime E: type) type {
         const Self = @This();
 
         start: []usize,
+        elist: []E,
         allocator: Allocator,
 
-        pub fn init(allocator: Allocator, n: usize, edges: []const std.meta.Tuple(&.{ usize, E })) Self {
+        pub fn init(allocator: Allocator, n: usize, edges: []const std.meta.Tuple(&.{ usize, E })) !Self {
             const self = Self{
-                .start = allocator.alloc(usize, n + 1),
+                .start = try allocator.alloc(usize, n + 1),
+                .elist = try allocator.alloc(E, edges.len),
                 .allocator = allocator,
             };
             @memset(self.start, 0);
@@ -20,7 +22,13 @@ pub fn Csr(comptime E: type) type {
             for (1..n + 1) |i| {
                 self.start[i] += self.start[i - 1];
             }
-            // TODO: implement
+            var counter = try allocator.alloc(usize, n + 1);
+            defer allocator.free(counter);
+            @memcpy(counter, self.start);
+            for (edges) |e| {
+                self.elist[counter[e.@"0"]] = e.@"1";
+                counter[e.@"0"] += 1;
+            }
             return self;
         }
         pub fn deinit(self: *Self) void {
