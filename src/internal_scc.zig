d525834e6d754ff51e852582cb75257bf3196cb7
Ryoga-exe <git@ryoga.dev>
2024-04-09 19:34:06 +0900

    feat (wip): implement sccIds and dfs

--- src/internal_scc.zig
+++ src/internal_scc.zig
@@ -8,6 +8,15 @@ pub const SccGraph = struct {
     const Edge = struct {
         to: usize,
     };
+    const Env = struct {
+        g: Csr(Edge),
+        now_ord: usize,
+        group_num: usize,
+        visited: std.ArrayList(usize),
+        low: []usize,
+        ord: []?usize,
+        ids: []usize,
+    };
 
     n: usize,
     edges: std.ArrayList(std.meta.Tuple(.{ usize, Edge })),
@@ -30,23 +39,15 @@ pub const SccGraph = struct {
     pub fn addEdge(self: *Self, from: usize, to: usize) !void {
         try self.edges.append(.{ from, Edge{ .to = to } });
     }
-    pub fn sccIds(self: Self) std.meta.Tuple(&.{ usize, usize }) {
-        var env = struct {
-            g: Csr(Edge),
-            now_ord: usize,
-            group_num: usize,
-            visited: []usize,
-            low: []usize,
-            ord: []?usize,
-            ids: []usize,
-        }{
+    pub fn sccIds(self: Self) !std.meta.Tuple(&.{ usize, []usize }) {
+        var env = Env{
             .g = Csr(Edge).init(self.allocator, self.n, self.edges, Edge{ .to = 0 }),
             .now_ord = 0,
             .group_num = 0,
-            .visited = self.allocator.alloc(usize, self.n),
-            .low = self.allocator.alloc(usize, self.n),
-            .ord = self.allocator.alloc(?usize, self.n),
-            .ids = self.allocator.alloc(usize, self.n),
+            .visited = std.ArrayList(usize).init(self.allocator),
+            .low = try self.allocator.alloc(usize, self.n),
+            .ord = try self.allocator.alloc(?usize, self.n),
+            .ids = try self.allocator.alloc(usize, self.n),
         };
 
         @memset(env.low, 0);
@@ -54,10 +55,48 @@ pub const SccGraph = struct {
         @memset(env.ids, 0);
 
         defer {
-            self.allocator.free(env.visited);
+            env.visited.deinit();
             self.allocator.free(env.low);
             self.allocator.free(env.ord);
             self.allocator.free(env.ids);
+        }
+
+        for (0..self.n) |i| {
+            if (env.ord[i] == null) {
+                try self.dfs(i, self.n, &env);
+            }
+        }
+        for (0..self.n) |i| {
+            env.ids[i] = env.group_num - 1 - env.ids[i];
+        }
+        return .{ env.group_num, self.allocator.dupe(usize, env.ids) };
+    }
+    fn dfs(v: usize, n: usize, env: *Env) !void {
+        env.low[v] = env.now_ord;
+        env.ord[v] = env.now_ord;
+        env.now_ord += 1;
+        try env.visited.append(v);
+
+        for (env.g.start[v]..env.g.start[v + 1]) |i| {
+            const to = env.g.elist[i].to;
+            if (env.ord[to]) |ord| {
+                env.low[v] = @min(env.low[v], ord);
+            } else {
+                dfs(to, n, &env);
+                env.low[v] = @min(env.low[v], env.low[to]);
+            }
+        }
+        if (env.low[v] == env.ord[v].?) {
+            while (true) {
+                const u = env.visited.getLast();
+                env.visited.pop();
+                env.ord[u] = n;
+                env.ids[u] = env.group_num;
+                if (u == v) {
+                    break;
+                }
+            }
+            env.group_num += 1;
         }
     }
 };
