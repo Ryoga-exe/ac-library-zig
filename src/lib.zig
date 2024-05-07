f49a4ad60485860427663858876b36e43f30ba50
Ryoga-exe <git@ryoga.dev>
2024-05-07 09:40:04 +0900

    refactor

--- src/lib.zig
+++ src/lib.zig
@@ -1,5 +1,5 @@
 const std = @import("std");
-pub const Dsu = @import("dsu.zig").Dsu;
+pub const Dsu = @import("dsu.zig");
 pub const FenwickTree = @import("fenwicktree.zig").FenwickTree;
 pub const FenwickTreeI64 = @import("fenwicktree.zig").FenwickTreeI64;
 pub const FenwickTreeI32 = @import("fenwicktree.zig").FenwickTreeI32;
