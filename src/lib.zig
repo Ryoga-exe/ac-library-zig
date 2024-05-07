fbdf4761753ce4d3c339d4f7b422a661799f493f
Ryoga-exe <git@ryoga.dev>
2024-05-07 18:36:28 +0900

    fix: lib.zig

--- src/lib.zig
+++ src/lib.zig
@@ -3,7 +3,7 @@ pub const Dsu = @import("dsu.zig");
 pub const FenwickTree = @import("fenwicktree.zig").FenwickTree;
 pub const FenwickTreeI64 = @import("fenwicktree.zig").FenwickTreeI64;
 pub const FenwickTreeI32 = @import("fenwicktree.zig").FenwickTreeI32;
-pub const SccGraph = @import("scc.zig").SccGraph;
+pub const SccGraph = @import("scc.zig");
 pub const Segtree = @import("segtree.zig").Segtree;
 
 comptime {
