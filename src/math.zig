a23ecdd6241cb8531196bc2558b9e6bc3f9176f5
Ryoga-exe <git@ryoga.dev>
2024-05-08 14:47:51 +0900

    feat: implement crt

--- src/math.zig
+++ src/math.zig
@@ -76,3 +76,86 @@ test invMod {
     try std.testing.expectEqual(@as(i64, 1), @mod(invMod(123, 1_000_000_007) * 123, 1_000_000_007));
     try std.testing.expectEqual(@as(i64, 1), @mod(invMod(456, 1_000_000_007) * 456, 1_000_000_007));
 }
+
+pub fn crt(r: []const i64, m: []const i64) struct { i64, i64 } {
+    assert(r.len == m.len);
+
+    // Contracts: 0 <= r0 < m0
+    var r0: i64 = 0;
+    var m0: i64 = 1;
+    for (r, m) |rr, mm| {
+        assert(1 <= mm);
+        var ri = @mod(rr, mm);
+        var mi = mm;
+        if (m0 < mi) {
+            std.mem.swap(i64, &r0, &ri);
+            std.mem.swap(i64, &m0, &mi);
+        }
+        if (@mod(m0, mi) == 0) {
+            if (@mod(r0, mi) != ri) {
+                return .{ 0, 0 };
+            }
+            continue;
+        }
+        // assume: m0 > mi, lcm(m0, mi) >= 2 * max(m0, mi)
+
+        // (r0, m0), (ri, mi) -> (r2, m2 = lcm(m0, m1));
+        // r2 % m0 = r0
+        // r2 % mi = ri
+        // -> (r0 + x*m0) % mi = ri
+        // -> x*u0*g = ri-r0 (mod u1*g) (u0*g = m0, u1*g = mi)
+        // -> x = (ri - r0) / g * inv(u0) (mod u1)
+
+        // im = inv(u0) (mod u1) (0 <= im < u1)
+        const gim = internal.invGcd(m0, mi);
+        const g = gim.@"0";
+        const im = gim.@"1";
+        const ui = @divFloor(mi, g);
+        // |ri - r0| < (m0 + mi) <= lcm(m0, mi)
+        if (@mod(ri - r0, g) != 0) {
+            return .{ 0, 0 };
+        }
+        // u1 * u1 <= mi * mi / g / g <= m0 * mi / g = lcm(m0, mi)
+        const x = @mod(@mod(@divFloor(ri - r0, g), ui) * im, ui);
+
+        // |r0| + |m0 * x|
+        // < m0 + m0 * (u1 - 1)
+        // = m0 + m0 * mi / g - m0
+        // = lcm(m0, mi)
+        r0 += x * m0;
+        m0 *= ui; // -> lcm(m0, mi)
+        if (r0 < 0) {
+            r0 += m0;
+        }
+    }
+
+    return .{ r0, m0 };
+}
+
+test crt {
+    const tests = [_]struct { a: []const i64, b: []const i64, expected: struct { i64, i64 } }{
+        .{
+            .a = &[_]i64{ 44, 23, 13 },
+            .b = &[_]i64{ 13, 50, 22 },
+            .expected = .{ 1773, 7150 },
+        },
+        .{
+            .a = &[_]i64{ 12345, 67890, 99999 },
+            .b = &[_]i64{ 13, 444321, 95318 },
+            .expected = .{ 103333581255, 550573258014 },
+        },
+        .{
+            .a = &[_]i64{ 0, 3, 4 },
+            .b = &[_]i64{ 1, 9, 5 },
+            .expected = .{ 39, 45 },
+        },
+    };
+
+    for (tests) |t| {
+        const expected = t.expected;
+        const result = crt(t.a, t.b);
+
+        try std.testing.expectEqual(expected.@"0", result.@"0");
+        try std.testing.expectEqual(expected.@"1", result.@"1");
+    }
+}
