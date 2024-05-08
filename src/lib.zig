cb5803179f0fa6c410a3ee0f0719fc11d42b3adc
Ryoga-exe <git@ryoga.dev>
2024-05-08 21:23:18 +0900

    feat: add lib

--- src/lib.zig
+++ src/lib.zig
@@ -5,6 +5,10 @@ pub const FenwickTreeI64 = @import("fenwicktree.zig").FenwickTreeI64;
 pub const FenwickTreeI32 = @import("fenwicktree.zig").FenwickTreeI32;
 pub const SccGraph = @import("scc.zig");
 pub const Segtree = @import("segtree.zig").Segtree;
+pub const powMod = @import("math.zig").powMod;
+pub const invMod = @import("math.zig").invMod;
+pub const crt = @import("math.zig").crt;
+pub const floorSum = @import("math.zig").floorSum;
 
 comptime {
     std.testing.refAllDecls(@This());
