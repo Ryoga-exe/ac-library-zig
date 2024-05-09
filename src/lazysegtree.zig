a0ee4fee383323dfffe671a77957574381185370
Ryoga-exe <git@ryoga.dev>
2024-05-09 13:01:02 +0900

    feat: add init and deinit

--- src/lazysegtree.zig
+++ src/lazysegtree.zig
@@ -1,17 +1,71 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
+const internal = @import("internal_bit.zig");
 
-pub fn LazySegtree() type {
+pub fn LazySegtree(
+    comptime S: type,
+    comptime op: fn (S, S) S,
+    comptime e: fn () S,
+    comptime F: type,
+    comptime mapping: fn (F, S) S,
+    comptime composition: fn (F, F) F,
+    comptime id: fn () F,
+) type {
+    _ = composition;
+    _ = mapping;
     return struct {
         const Self = @This();
 
+        n: usize,
+        size: usize,
+        log: usize,
+        d: []S,
+        lz: []F,
         allocator: Allocator,
 
-        pub fn init(allocator: Allocator) !Self {
+        pub fn init(allocator: Allocator, n: usize) !Self {
+            const size = internal.bitCeil(n);
+            const log = @ctz(size);
             const self = Self{
+                .n = n,
+                .size = size,
+                .log = log,
+                .d = try allocator.alloc(S, 2 * size),
+                .lz = try allocator.alloc(F, size),
                 .allocator = allocator,
             };
+            @memset(self.d, e());
+            @memset(self.lz, id());
             return self;
+        }
+        pub fn initFromSlice(allocator: Allocator, v: []const S) !Self {
+            const n = v.len;
+            const size = internal.bitCeil(n);
+            const log = @ctz(size);
+            var self = Self{
+                .n = n,
+                .size = size,
+                .log = log,
+                .d = try allocator.alloc(S, 2 * size),
+                .lz = try allocator.alloc(F, size),
+                .allocator = allocator,
+            };
+            @memset(self.d, e());
+            for (0..n) |i| {
+                self.d[size + i] = v[i];
+            }
+            var i: usize = size - 1;
+            while (i >= 1) : (i -= 1) {
+                self.update(i);
+            }
+            return self;
+        }
+        pub fn deinit(self: *Self) void {
+            self.allocator.free(self.d);
+            self.allocator.free(self.lz);
+        }
+        fn update(self: *Self, k: usize) void {
+            self.d[k] = op(self.d[2 * k], self.d[2 * k + 1]);
         }
     };
 }
