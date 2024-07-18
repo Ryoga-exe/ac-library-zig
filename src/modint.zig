25d25214dbfc53c842fc4aa7f3e2732c35d0ee26
Ryoga-exe <git@ryoga.dev>
2024-07-18 17:59:23 +0900

    feat (wip): implement StaticModint

--- src/modint.zig
+++ src/modint.zig
@@ -1,15 +1,17 @@
 const std = @import("std");
+const internal = @import("internal_math.zig");
 
 pub fn StaticModint(comptime m: comptime_int) type {
     if (m < 1) {
         @compileError("m must be greater than or equal to 1");
     }
 
-    const T = std.math.IntFittingRange(0, m - 1);
-    const U = std.math.IntFittingRange(0, (m - 1) * (m - 1));
+    const T = comptime std.math.IntFittingRange(0, m - 1);
+    const U = comptime std.math.IntFittingRange(0, (m - 1) * (m - 1));
 
     return struct {
         const Self = @This();
+        const prime = internal.comptimeIsPrime(m);
 
         v: T,
 
@@ -59,16 +61,41 @@ pub fn StaticModint(comptime m: comptime_int) type {
                 .v = std.math.comptimeMod(self.v * x, m),
             };
         }
+
+        pub fn pow(self: Self, n: anytype) Self {
+            std.debug.assert(0 <= n);
+            var x: Self = self;
+            var r: Self = Self.raw(1);
+            var _n: std.math.IntFittingRange(0, n) = n;
+            while (_n > 0) : (_n >>= 1) {
+                if (_n & 1 == 1) {
+                    r = r.mul(x);
+                }
+                x = x.mul(x);
+            }
+            return r;
+        }
+
+        pub fn inv(self: Self) Self {
+            if (prime) {
+                return self.pow(m - 2);
+            } else {
+                const g, const x = internal.invGcd(self.v, m);
+                std.debug.assert(g == 1);
+                return x;
+            }
+        }
     };
 }
 
 const DynamicModint = struct {};
 
 test StaticModint {
-    var s = StaticModint(12).init(0);
+    var s = StaticModint(13).init(0);
     std.debug.print("{}\n", .{s.v});
     s = s.add(1).add(1);
-    s = s.add(s).mul(2);
-    s = s.add(s).sub(10);
+    std.debug.print("{}\n", .{s.v});
+    s = s.pow(5);
+    s = s.inv();
     std.debug.print("{}\n", .{s.v});
 }
