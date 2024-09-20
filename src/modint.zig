c46aefa6f688593474b44f2691c21018553fe50e
Ryoga-exe <git@ryoga.dev>
2024-09-20 17:56:08 +0900

    feat (wip): add test

--- src/modint.zig
+++ src/modint.zig
@@ -15,17 +15,16 @@ pub fn StaticModint(comptime m: comptime_int) type {
 
         v: T,
 
+        pub fn mod(_: Self) comptime_int {
+            return m;
+        }
+
         pub fn raw(v: anytype) Self {
             return Self{ .v = @intCast(v) };
         }
 
         pub fn init(v: anytype) Self {
             return Self{ .v = std.math.comptimeMod(v, m) };
-        }
-
-        pub fn mod(comptime self: Self) comptime_int {
-            _ = self; // autofix
-            return m;
         }
 
         pub fn add(self: Self, v: anytype) Self {
@@ -62,6 +61,18 @@ pub fn StaticModint(comptime m: comptime_int) type {
             };
         }
 
+        pub fn div(self: Self, v: anytype) Self {
+            const x: U = switch (@TypeOf(v)) {
+                Self => v.inv(),
+                else => Self.init(v).inv().v,
+            };
+            return self.mul(x);
+        }
+
+        pub fn negate(self: Self) Self {
+            return Self.raw(0).sub(self.v);
+        }
+
         pub fn pow(self: Self, n: anytype) Self {
             std.debug.assert(0 <= n);
             var x: Self = self;
@@ -88,7 +99,14 @@ pub fn StaticModint(comptime m: comptime_int) type {
     };
 }
 
-const DynamicModint = struct {};
+pub const DynamicModint = struct {
+    const Self = @This();
+
+    pub fn init() Self {}
+};
+
+pub const Modint998244353 = StaticModint(998244353);
+pub const Modint1000000007 = StaticModint(1000000007);
 
 test StaticModint {
     var s = StaticModint(13).init(0);
@@ -98,4 +116,34 @@ test StaticModint {
     s = s.pow(5);
     s = s.inv();
     std.debug.print("{}\n", .{s.v});
+
+    // init
+    try std.testing.expectEqual(Modint1000000007.init(0).v, 0);
+    try std.testing.expectEqual(Modint1000000007.init(1).v, 1);
+    try std.testing.expectEqual(Modint1000000007.init(1_000_000_008).v, 1);
+    try std.testing.expectEqual(Modint1000000007.init(-1).v, 1_000_000_006);
+
+    // add
+    try std.testing.expectEqual(Modint1000000007.init(1).add(1).v, 2);
+    try std.testing.expectEqual(Modint1000000007.init(1_000_000_006).add(2).v, 1);
+
+    // sub
+    try std.testing.expectEqual(Modint1000000007.init(2).sub(1).v, 1);
+    try std.testing.expectEqual(Modint1000000007.init(0).sub(1).v, 1_000_000_006);
+
+    // mul
+    try std.testing.expectEqual(Modint1000000007.init(1).mul(1).v, 1);
+    try std.testing.expectEqual(Modint1000000007.init(2).mul(2).v, 4);
+    try std.testing.expectEqual(Modint1000000007.init(100_000).mul(100_000).v, 999_999_937);
+
+    // div
+    try std.testing.expectEqual(Modint1000000007.init(0).div(1).v, 0);
+    try std.testing.expectEqual(Modint1000000007.init(1).div(1).v, 1);
+    try std.testing.expectEqual(Modint1000000007.init(2).div(2).v, 1);
+    try std.testing.expectEqual(Modint1000000007.init(1).div(42).v, 23_809_524);
+
+    // sum
+    // product
+    // binop_coercion
+    // assign_coercion
 }
