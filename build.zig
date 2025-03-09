f30af6f7e2bd965fcbe2e3c353d7da7e485ee2f4
Ryoga-exe <git@ryoga.dev>
2025-03-09 22:18:33 +0900

    chore: update build.zig

--- build.zig
+++ build.zig
@@ -18,10 +18,6 @@ pub fn build(b: *std.Build) void {
 
     b.installArtifact(lib);
 
-    _ = b.addModule("ac_library", .{
-        .root_source_file = b.path("src/lib.zig"),
-    });
-
     const test_step = b.step("test", "Run library tests");
     const unit_tests = b.addTest(.{
         .root_module = mod,
