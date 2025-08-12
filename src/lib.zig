5da238fb3042e161666385d54f8bf444f6aa8d67
Ryoga-exe <git@ryoga.dev>
2025-08-12 22:31:27 +0900

    feat: add lib

--- src/lib.zig
+++ src/lib.zig
@@ -11,6 +11,11 @@ pub const LazySegtreeNS = @import("lazysegtree.zig").LazySegtreeFromNS;
 pub const MfGraph = @import("maxflow.zig").MfGraph;
 pub const McfGraph = @import("mincostflow.zig").McfGraph;
 pub const TwoSat = @import("twosat.zig");
+pub const StaticModint = @import("modint.zig").StaticModint;
+pub const Modint1000000007 = @import("modint.zig").Modint1000000007;
+pub const Modint998244353 = @import("modint.zig").Modint998244353;
+pub const DynamicModint = @import("modint.zig").DynamicModint;
+pub const Modint = @import("modint.zig").Modint;
 
 pub usingnamespace @import("math.zig");
 pub usingnamespace @import("string.zig");
